8020dffc6cda7717a9f3fe199b6de1eb5c574392
Jamie <jamiemoulding@outlook.com>
2026-04-22 13:04:01 +0100

    OFFLINE FIXES

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,60 +1,26 @@
-const CACHE_NAME = "staff-app-v1";
+const CACHE = "zorvia-v3";
 
-const urlsToCache = [
-  "/",
-  "/index.html",
-  "/manifest.json",
-];
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) =>
+      cache.addAll([
+        "/",
+        "/index.html",
+        "/manifest.json"
+      ])
+    )
+  );
+  self.skipWaiting();
+});
 
-/* INSTALL */
-self.addEventListener(
-  "install",
-  (event) => {
-    event.waitUntil(
-      caches.open(CACHE_NAME).then((cache) =>
-        cache.addAll(urlsToCache)
-      )
-    );
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
 
-    self.skipWaiting();
-  }
-);
-
-/* ACTIVATE */
-self.addEventListener(
-  "activate",
-  (event) => {
-    event.waitUntil(
-      caches.keys().then((keys) =>
-        Promise.all(
-          keys.map((key) => {
-            if (key !== CACHE_NAME) {
-              return caches.delete(key);
-            }
-          })
-        )
-      )
-    );
-
-    self.clients.claim();
-  }
-);
-
-/* FETCH */
-self.addEventListener(
-  "fetch",
-  (event) => {
-    event.respondWith(
-      caches.match(event.request).then(
-        (cached) => {
-          return (
-            cached ||
-            fetch(event.request).catch(() =>
-              caches.match("/index.html")
-            )
-          );
-        }
-      )
-    );
-  }
-);
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((res) => {
+      return res || fetch(event.request);
+    })
+  );
+});
